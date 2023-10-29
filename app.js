a9952f7d291629b4e9235e046b4aca05065c2d9f
rbeyzas <rukiyebeyzasarikaya@hotmail.com>
2023-10-29 16:18:00 +0300

    MongoDB connect and user model

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 const { ApolloServer } = require('@apollo/server');
+require('dotenv').config();
+const mongoose = require('mongoose');
 const gql = require('graphql-tag');
 const { startStandaloneServer } = require('@apollo/server/standalone');
 const { readFileSync } = require('fs');
-const path = require('path'); // Dosya yollarını işlemek için path modülünü içe aktarıyoruz
+const path = require('path');
 
-// GraphQL şema dosyasının tam yolunu belirle
-const schemaPath = path.join(__dirname, 'graphql/types/schema.graphql');
+const schemaPath = path.join(__dirname, './schema/schema.gql');
 const typeDefs = readFileSync(schemaPath, 'utf8');
 
-// Resolver dosyasının tam yolunu belirle
-const resolversPath = path.join(__dirname, 'graphql/resolvers/index');
+const resolversPath = path.join(__dirname, './graphql/resolvers/index');
 const resolvers = require(resolversPath);
+
+mongoose
+  .connect(process.env.DB_URI, { useNewUrlParser: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((e) => console.log(e));
 
 const server = new ApolloServer({
   typeDefs,
